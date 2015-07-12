039c174da52eacd9aaaec825c2c7b96f3d1e2e70
TemplarRei <boris.stoyanovv@gmail.com>
2015-07-12 12:46:17 +0300

    Refactored somewhat the code, added GameProcessess.cs and PrintManager.cs. Extracted some methods, but most want more refacturing - there are a few methods that do waaay more than one thing.

---
+++ BattleField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleFiled
+{
+    class BattleField
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize;
+            string[,] battleField;
+            GameProcessess.BoardInit(out fieldSize, out battleField);
+            GameProcessess.GameTurns(fieldSize, battleField);
+        }
+    }
+}
